9872a1ca6db4977a84e8b3cd7a3d03201ccaa9a1
Mike Street <mike@liquidlight.co.uk>
2021-09-16 15:39:15 +0100

    FE: Begin styling of module

--- Resources/Public/JavaScript/FeUsersDataTable.js
+++ Resources/Public/JavaScript/FeUsersDataTable.js
@@ -1,51 +1,74 @@
 define([
-    "jquery", 
-    "datatables.net", 
-    "datatables.net-buttons", 
-    "datatables.net-buttons-print", 
-    "datatables.net-buttons-html5"
+	"jquery",
+	"datatables.net",
+	"datatables.net-buttons",
+	"datatables.net-buttons-print",
+	"datatables.net-buttons-html5"
 ], function() {
 
-    function initializeDataTable(filters) {
-        return $("#feusers-table").DataTable({
-            "processing": true,
-            "serverSide": true,
-            "order": [[ 0, "desc" ]],
-            "dom": "B<'form-inline form-inline-spaced'<'form-group'l><'form-group'f>rtip>",
-            "buttons": [
-                "csv", "print"
-            ],
-            "ajax": {
-                "url": TYPO3.settings.ajaxUrls["backend_modules_datatables_get_fe_users"],
-                "data": {
-                    "filters": filters,
-                }
-            },
-            "language": {
-                "emptyTable": "No data available in table"
-            },
-        });
-    }
+	function initializeDataTable(filters) {
+		return $("#feusers-table").DataTable({
+			"processing": true,
+			"serverSide": true,
+			"order": [[ 0, "desc" ]],
+			"dom": "<'form-inline form-inline-spaced'lf>rtipB",
+			"buttons": [
+				"csv"
+			],
+			"pagingType" : "full_numbers",
+			"ajax": {
+				"url": TYPO3.settings.ajaxUrls["backend_modules_datatables_get_fe_users"],
+				"data": {
+					"filters": filters,
+				}
+			},
+			"language": {
+				"emptyTable": "No data available in table"
+			},
+			"lengthMenu": [
+				[
+					10,
+					25,
+					50,
+					100,
+					-1
+				],
+				[
+					10,
+					25,
+					50,
+					100,
+					"All"
+				]
+			],
+			"classes": {
+				"sLength": "form-group",
+				"sLengthSelect": "form-control input-sm",
+				"sFilter": "form-group",
+				"sFilterInput": "form-control input-sm"
+			}
+		});
+	}
 
-    $(document).ready(function() {
-        var filters = {};
+	$(document).ready(function() {
+		var filters = {};
 
-        // Initialize the view
-        initializeDataTable(filters);
+		// Initialize the view
+		initializeDataTable(filters);
 
-        // Destroy the table and reinit with year filter
-        $(".searchUsergroups").click(function() {
-            var groups = [];
-            $('.usergroups input:checked').each(function() {
-                groups.push($(this).val());
-            });
-            $("#feusers-table").DataTable().destroy();
+		// Destroy the table and reinit with year filter
+		$(".searchUsergroups").click(function() {
+			var groups = [];
+			$('.usergroups input:checked').each(function() {
+				groups.push($(this).val());
+			});
+			$("#feusers-table").DataTable().destroy();
 
-            filters['usergroup'] = groups;
+			filters['usergroup'] = groups;
 
-            initializeDataTable(filters);
-        });
-    });
+			initializeDataTable(filters);
+		});
+	});
 
 
 });
