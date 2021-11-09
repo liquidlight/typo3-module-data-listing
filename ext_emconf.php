5d4529ede7ba8225e09e2a77185a97f2bd3f5ebf
Zaq Mughal <zaq@liquidlight.co.uk>
2021-11-09 11:35:12 +0000

    BE: Prepare module for extension listing

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,11 @@ $EM_CONF[$_EXTKEY] = [
 	'author' => 'Zaq Mughal',
 	'author_company' => 'Liquid Light Ltd',
 	'author_email' => 'zaq@liquidlight.co.uk',
-	'state' => 'stable',
-	'version' => '1.0.0',
+	'state' => 'beta',
+	'version' => '0.1.0',
 	'constraints' => [
 		'depends' => [
+			'typo3' => '9.5.0-9.5.99'
 		],
 		'conflicts' => [
 		],
