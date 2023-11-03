5fe06da61cacfebf517a2008967f8f89ab95cdf5
Mike Street <mike@liquidlight.co.uk>
2023-11-03 12:42:52 +0000

    fix: Add default icon for module group

--- ext_tables.php
+++ ext_tables.php
@@ -12,6 +12,7 @@ if (TYPO3_MODE === 'BE') {
 		[
 			'access' => 'user,group',
 			'name' => 'datalisting',
+			'iconIdentifier' => 'modulegroup-system',
 			'labels' => 'LLL:EXT:module_data_listing/Resources/Private/Language/locallang_mod_datalisting.xlf',
 		]
 	);
